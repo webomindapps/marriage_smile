0a20f75f60bfafb582f27be84552c36cf8b639a8
DhruvWebomindapps <138429149+DhruvWebomindapps@users.noreply.github.com>
2025-01-02 12:06:14 +0530

    02-01-2025

--- app/Models/Plan.php
+++ app/Models/Plan.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'name',
+        'duration',
+        'description',
+        'price',
+        'thumbnail',
+        'position',
+        'status'
+    ];
 }
